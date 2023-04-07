88a856890eddf2c7494f10c1378b38d3303023bc
EnteSuessSauer <thisonerandom@gmail.com>
2023-04-07 14:02:11 +0200

    .

--- Maximum Independent Set/src/Graph.java
+++ Maximum Independent Set/src/Graph.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
     private int V;   // number of vertices
     private boolean[][] adj;   // adjacency matrix
@@ -44,5 +47,15 @@ public class Graph {
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(2, 3);
+        graph.addEdge(2, 4);
+        graph.print();
     }
 }

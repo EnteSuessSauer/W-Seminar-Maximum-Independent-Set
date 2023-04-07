b0b3175c72bddd23338d5620edb263c99b442966
EnteSuessSauer <thisonerandom@gmail.com>
2023-04-07 02:59:52 +0200

    .

--- Maximum Independent Set/src/Graph.java
+++ Maximum Independent Set/src/Graph.java
@@ -45,14 +45,4 @@ public class Graph {
             System.out.println();
         }
     }
-
-    public static void main(String[] args) {
-        Graph graph = new Graph(5);
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(2, 3);
-        graph.addEdge(2, 4);
-        graph.print();
-    }
 }

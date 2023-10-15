f4d2d84a5b6b17f3973696707151a8be83783166
EnteSuessSauer <thisonerandom@gmail.com>
2023-10-15 19:57:44 +0200

    Schöner Wohnen

--- Maximum Independent Set/src/MISAlgorithm.java
+++ Maximum Independent Set/src/MISAlgorithm.java
@@ -9,10 +9,12 @@ public class MISAlgorithm {
         graph.addEdge(2, 3);
         graph.addEdge(2, 4);
         graph.print();
-        
+
+        System.out.println("Greedy: ");
         Greedy GreedyAlg = new Greedy(graph);
         graph.printMIS(GreedyAlg.runAlg());
 
+        System.out.println("Bron-Kerbosch: ");
         BronKerbosch BKAlg = new BronKerbosch(graph);
         List<List<Integer>>IndependentSets = BKAlg.runAlg();
         for (List<Integer> list : IndependentSets) {

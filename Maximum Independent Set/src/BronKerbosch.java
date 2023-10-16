251cb158d5959c6ede2735a68ff734734a5fe3ae
EnteSuessSauer <thisonerandom@gmail.com>
2023-10-16 21:08:28 +0200

    Warum geht das nicht? ;-;

--- Maximum Independent Set/src/BronKerbosch.java
+++ Maximum Independent Set/src/BronKerbosch.java
@@ -3,35 +3,43 @@ import java.util.List;
 
 public class BronKerbosch {
     private Graph graph;
+    private List<Integer> P;
+    private List<List<Integer>> MISets;  
     public BronKerbosch(Graph graph) {
         this.graph = graph;
+        this.P = new ArrayList<Integer>();
+        for (int i = 0; i < P.size();) {
+            P.add(i);
+        }
+
+        MISets = new ArrayList<List<Integer>>();
     }
 
     public List<List<Integer>> runAlg() {
-        return bronKerbosch(null, null, null);
+        return bronKerbosch(new ArrayList<Integer>(), P, new ArrayList<Integer>());
     }
 
     public List<List<Integer>> bronKerbosch(List<Integer> R, List<Integer> P, List<Integer> X) {
-        
-        List<List<Integer>> MISets = new ArrayList<>();
-        
         if (P.isEmpty() && X.isEmpty()) {
-            return null;
+            MISets.add(new ArrayList<>(R));
         } else {
             List<Integer> pCopy = new ArrayList<>(P);
             for (int v : pCopy) {
                 List<Integer> vNeighbors = graph.getNeighbors(v);
-                R.add(v);
-                P.retainAll(vNeighbors);
-                X.retainAll(vNeighbors);
+                List<Integer> Rnew = new ArrayList<>(R);
+                Rnew.add(v);
+                List<Integer> Pnew = new ArrayList<>(P);
+                Pnew.retainAll(vNeighbors);
+                List<Integer> Xnew = new ArrayList<>(X);
+                Xnew.retainAll(vNeighbors);
 
-                bronKerbosch(R, P, X);
+                bronKerbosch(Rnew, Pnew, Xnew);  
 
-                pCopy.remove(v);
+                pCopy.remove((Integer) v);
                 X.add(v);
-                // R.remove(v);
             }
-            return new ArrayList<>();
         }
+
+        return MISets;
     }
 }

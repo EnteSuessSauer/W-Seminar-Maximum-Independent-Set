316f4b929c57f1fb3448fea8885084836bce1394
EnteSuessSauer <thisonerandom@gmail.com>
2023-10-22 22:22:39 +0200

    neue Graphen

--- Maximum Independent Set/src/BronKerbosch.java
+++ Maximum Independent Set/src/BronKerbosch.java
@@ -1,10 +1,12 @@
+import java.time.Clock;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BronKerbosch {
     private Graph graph;
     private List<Integer> V;
-    private List<List<Integer>> MISets;  
+    private List<List<Integer>> MISets;
+    private Clock clock;  
     public BronKerbosch(Graph graph) {
         this.graph = graph;
         this.V = new ArrayList<Integer>();
@@ -12,16 +14,20 @@ public class BronKerbosch {
             V.add(i);
         }
 
-        System.out.println(V);
+        // System.out.println(V);
 
         MISets = new ArrayList<List<Integer>>();
+
+        clock = Clock.systemDefaultZone();
     }
 
     public List<List<Integer>> runAlg() {
         graph.inverseGraph();
         System.out.println("Sarting Bron-Kerbosch-Algorithm");
+        long startingTime = clock.millis();
         List<List<Integer>> MISCandidates = bronKerbosch(new ArrayList<Integer>(), V, new ArrayList<Integer>());
-        System.out.println("Finished running");
+        long elapsedTime = clock.millis() - startingTime;
+        System.out.println("Finished running in " + elapsedTime + " milliseconds");
         graph.inverseGraph();
 
         int maxSize = 0;
@@ -38,24 +44,26 @@ public class BronKerbosch {
             }
         }
 
+        System.out.println("Size of the Maximum Independent Sets: " + maxSize);
+
         return MIS;
     }
 
     public List<List<Integer>> bronKerbosch(List<Integer> R, List<Integer> P, List<Integer> X) {
-        System.out.println("");
-        System.out.println("Recursive: " + "R: " + R + " P: " + P + " X: " + X);
+        // System.out.println("");
+        // System.out.println("Recursive: " + "R: " + R + " P: " + P + " X: " + X);
         if (P.isEmpty() && X.isEmpty()) {
-            System.out.println("P & X are empty");
+            // System.out.println("P & X are empty");
             List<Integer> MIS = new ArrayList<>(R);
-            System.out.println("Reported MIS: " + MIS);
-            System.out.println("");
+            // System.out.println("Reported MIS: " + MIS);
+            // System.out.println("");
             MISets.add(MIS);
             return MISets;
         } else {
-            System.out.println("P & X are not empty");
+            // System.out.println("P & X are not empty");
             List<Integer> pCopy = new ArrayList<>(P);
             for (int v : pCopy) {
-                System.out.println("Iterated Vertex: " + v);
+                // System.out.println("Iterated Vertex: " + v);
                 List<Integer> vNeighbors = graph.getNeighbors(v);
                 List<Integer> Rnew = new ArrayList<>(R);
                 Rnew.add(v);
@@ -67,7 +75,7 @@ public class BronKerbosch {
 
                 P.remove((Integer) v);
                 X.add((Integer) v);
-                System.out.println("Backtracking " + P + " " + X);
+                // System.out.println("Backtracking " + P + " " + X);
             }
         }
 
--- Maximum Independent Set/src/greedy.java
+++ Maximum Independent Set/src/greedy.java
@@ -9,25 +9,26 @@ public class Greedy {
     }
 
     public List<Integer> runAlg() {
-        
+
         List<Integer> independentSet = new ArrayList<Integer>();
-        List<Integer> degrees = new ArrayList<Integer>();
         int order = graph.getOrder();
+        int[] degrees = new int[order];
         boolean[] visited = new boolean[order];
         
         // Initialisiere degrees und visited
         for (int i = 0; i < order; i++) {
-            degrees.add(graph.getNeighbors(i).size());    // der Grad eines Knotens entspricht der Anzahl der Nachbarn
+            degrees[i] = graph.getNeighbors(i).size();    // der Grad eines Knotens entspricht der Anzahl der Nachbarn
             visited[i] = false;                     // Array für alle besuchten Knoten
         }
         
-        for (int i = 0; i < order; i++) {
+        for (int i = 0; i < order + 1; i++) {
 
             // Such den Knoten mit dem kleinsten Grad, der noch nicht besucht wurde
             int minDegree = Integer.MAX_VALUE;
             int minVertex = -1;
             for (int j = 0; j < order; j++) {
-                int currentDegree = degrees.get(j);
+                int currentDegree = degrees[j];
+                System.out.println(currentDegree);
                 if (!visited[j] && currentDegree < minDegree) {
                     minDegree = currentDegree;
                     minVertex = j;
@@ -49,9 +50,10 @@ public class Greedy {
             
             // Aktualisiere die Grade der Knoten
             for (int j = 0; j < order; j++) {
-                degrees.remove(j);
-                degrees.add(graph.getNeighborsUpdated(j, visited).size());
+                degrees[j] = graph.getNeighborsUpdated(j, visited).size();
             }
+
+            System.out.println("");
             
         }
 
